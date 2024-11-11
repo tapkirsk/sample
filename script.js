89ba1a8d8cbd6845ad45b10436baeee174425a1f
Shubham Tapkir <tapkirsk56@gmail.com>
2024-11-12 01:59:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+    document.querySelector(".login-btn").addEventListener("click", function() {
+        // Redirect to the login page or open a login modal
+        window.location.href = "index.html"; // Replace with your actual login page URL
+    });
+</script>
